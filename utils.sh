93afb0c701b9199a11a3700537525bceff39bcb2
Rafael Arias <45745870+rafael-ariascalles@users.noreply.github.com>
2023-12-20 11:43:39 +0000

    Login and Sigup Integration + Unit test

--- utils.sh
+++ utils.sh
@@ -1,5 +1,9 @@
-docker run --name some-postgres -p 5432:5432 -e POSTGRES_USER=taxi -e POSTGRES_DB=taxi -e POSTGRES_PASSWORD=taxi -d postgres
-docker run --name some-redis -p 6379:6379 -d redis
+# (env)$ django-admin startproject taxi .
+# (env)$ python manage.py startapp trips
+docker run --rm --name some-postgres -p 5432:5432 -e POSTGRES_USER=taxi -e POSTGRES_DB=taxi -e POSTGRES_PASSWORD=taxi -d postgres
+docker run --rm --name some-redis -p 6379:6379 -d redis
 docker exec -it some-redis redis-cli ping
 export PGDATABASE=taxi PGUSER=taxi PGPASSWORD=taxi
 python manage.py makemigrations
+docker start some-postgres
+docker start some-redis
